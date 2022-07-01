7398df13ff6ff5f6b409c908a1d132e91cf89e43
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-01 17:00:11 +0100

    js weather app

---
+++ js_weather_app/script.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector(".wrapper")
+inputPart = document.querySelector(".input-part")
+infoText = document.querySelector(".info-text")
+inputField = document.querySelector("input")
+
