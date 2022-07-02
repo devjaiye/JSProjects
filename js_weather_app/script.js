30463dcf02ae5b7b204121aa4dbb8fe73cedad40
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-02 19:24:45 +0100

    js weather app

--- js_weather_app/script.js
+++ js_weather_app/script.js
@@ -4,7 +4,9 @@ infoText = document.querySelector(".info-text")
 inputField = document.querySelector("input")
 apiKey = "5c37294524c56b2e84a0ae6df81e3e45"
 getLocatiionBtn = document.querySelector("button")
+arrowBack = wrapper.querySelector("header i")
 let api
+weatherIcon = document.querySelector("weather-part img")
 
 inputField.addEventListener("keyup", e=>{
     //...if user pressed enter btn input value is not empty 
@@ -26,7 +28,8 @@ getLocatiionBtn.addEventListener("click", ()=>{
 function onSuccess(position){
     // console.log(position)
     const {latitude, longitude} = position.coords //..getting the lat and long from coordinator object
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+    //..addded {&units=metric} to the api to round up the number
     fectchWeatherData()
 }
 
@@ -36,7 +39,7 @@ function onError(error){
 }
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}, &appid=${apiKey}`
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     fectchWeatherData()
 }
 
@@ -55,9 +58,40 @@ function weatherDetails(info){
         infoText.innerText = `You entered ${inputField.value} which isn't a valid city` //...if the user input a valid location, it throws error on the info text
     } else{
          //..get api data to properties in info-text   
+        const city = info.name
+        const country = info.sys.country
+        const {description, id} = info.weather[0]
+        const{feels_like,humidity, temp} = info.main
+
+        //...using custom icon accoriding to the id which api returns 
+          if(id == 800){
+             weatherIcon.src = "icons/clear.svg"
+          } else if(id >= 200 && id <= 232){
+             weatherIcon.src = "icons/storm.svg"
+          } else if(id >= 600 && id <= 622){
+            weatherIcon.src = "icons/snow.svg"
+          } else if(id >= 701 && id <= 781){
+             weatherIcon.src = "icons/haze.svg"
+          } else if(id >= 804 && id <= 801){
+            weatherIcon.src = "icons/cloud.svg"
+          }
+          else if((id >= 300 && id <= 321) || (id >= 500 && id <= 531)){
+            weatherIcon.src = "icons/rain.svg"
+          }
         
+        //..parse the above values into the html elements 
+        wrapper.querySelector(".temp, .numb").innerText = Math.floor(temp) //..round up number to nearest Integer
+        wrapper.querySelector(".weather").innerHTML = description
+        wrapper.querySelector(".location span").innerHTML = `${city}, ${country}`
+        wrapper.querySelector(".temp .numb-2").innerHTML = Math.floor(feels_like)
+        wrapper.querySelector(".humidity span").innerHTML = `${humidity}%`
+
         infoText.classList.remove("pending", "error") //..if we get the correct city from the api we hide pending and error message
         wrapper.classList.add("active") //..show the dashboard which displays the weather info
         console.log(info)
     }
 }
+
+arrowBack.addEventListener("click", ()=>{
+    wrapper.classList.remove("active")
+})
