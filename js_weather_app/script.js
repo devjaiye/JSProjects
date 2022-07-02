6e6941ac7d636ee9414158d98a1a57a8cc05b442
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-02 10:02:00 +0100

    js weather app

--- js_weather_app/script.js
+++ js_weather_app/script.js
@@ -2,4 +2,62 @@ const wrapper = document.querySelector(".wrapper")
 inputPart = document.querySelector(".input-part")
 infoText = document.querySelector(".info-text")
 inputField = document.querySelector("input")
+apiKey = "5c37294524c56b2e84a0ae6df81e3e45"
+getLocatiionBtn = document.querySelector("button")
+let api
 
+inputField.addEventListener("keyup", e=>{
+    //...if user pressed enter btn input value is not empty 
+    if(e.key == "Enter" && inputField.value !=""){
+        requestApi(inputField.value)
+    }
+})
+
+getLocatiionBtn.addEventListener("click", ()=>{
+    //.. The geolocation api is used to get the user's current location
+    if(navigator.geolocation){ //..if user's browser supports geolocation
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    } else {
+        alert("Browser doesn't support geolocation api")
+    }
+})
+
+//..if user allow the request, we'll get the latitiute and longitude of the user device
+function onSuccess(position){
+    // console.log(position)
+    const {latitude, longitude} = position.coords //..getting the lat and long from coordinator object
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    fectchWeatherData()
+}
+
+function onError(error){
+  infoText.innerText = error.message //..html text will display error message
+  infoText.classList.add("error")
+}
+
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}, &appid=${apiKey}`
+    fectchWeatherData()
+}
+
+function fectchWeatherData(){
+    infoText.innerText="Getting weather info..."
+    infoText.classList.add("pending")
+
+    //...getting api response from the server and returning it into our app parsing it as Js object
+    fetch(api).then(response => response.json()).
+    then(result => weatherDetails(result)) //...then functiion calling weatherDetails function passing api resulf
+}
+
+function weatherDetails(info){
+    infoText.classList.replace("pending", "error") //..our css style changes the text info background 
+    if(info.cod == "404"){ //..cod is an object called from the weather api
+        infoText.innerText = `You entered ${inputField.value} which isn't a valid city` //...if the user input a valid location, it throws error on the info text
+    } else{
+         //..get api data to properties in info-text   
+        
+        infoText.classList.remove("pending", "error") //..if we get the correct city from the api we hide pending and error message
+        wrapper.classList.add("active") //..show the dashboard which displays the weather info
+        console.log(info)
+    }
+}
