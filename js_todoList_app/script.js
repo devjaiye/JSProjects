24d2a66611897dfcfe27f59eea7bbc2700903bc8
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-06 23:29:20 +0100

    js_todoList_app

--- js_todoList_app/script.js
+++ js_todoList_app/script.js
@@ -5,9 +5,14 @@ taskInput.addEventListener("keyup", e =>{
     let userTask = taskInput.value.trim() //.. .trim prevents user from sending empty value 
     if(e.key == "Enter" && userTask){
         //..get localstorage 
-        let todos = localstorage.getItem("todo-list")
-        if(!todos){
+        let todos = JSON.parse(localstorage.getItem("todo-list"))
+        if(!todos){ //.. if todos isn't exist, pass an empty array to todos
             todos = []
         }
+        taskInput.value = ""
+        let taskInfo = {name: userTask, status: "pending"}
+        todos.push(taskInfo) //.. adding new task to todos
+        //..saving to todo-list name in local storage and convert todos to string
+        localStorage.setItem("todo-list", JSON.stringify(todos))
     }
 })
